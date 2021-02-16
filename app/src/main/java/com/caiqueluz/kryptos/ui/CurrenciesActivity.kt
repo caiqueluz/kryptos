cd495d3bcaed167d5703e95c805bca6491188708
caiqueluz <eucaiqueluz@gmail.com>
2021-02-16 19:33:29 -0300

    add loading progress bar on currencies activity
    Signed-off-by: caiqueluz <eucaiqueluz@gmail.com>

--- app/src/main/java/com/caiqueluz/kryptos/ui/CurrenciesActivity.kt
+++ app/src/main/java/com/caiqueluz/kryptos/ui/CurrenciesActivity.kt
@@ -1,8 +1,56 @@
 package com.caiqueluz.kryptos.ui
 
+import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.caiqueluz.kryptos.R
+import com.caiqueluz.kryptos.databinding.ActivityCurrenciesBinding
+import com.caiqueluz.kryptos.network.NetworkResponse.*
+import com.caiqueluz.kryptos.ui.viewmodel.CurrenciesVO
+import com.caiqueluz.kryptos.ui.viewmodel.CurrenciesViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class CurrenciesActivity : AppCompatActivity(R.layout.activity_currencies)
+class CurrenciesActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityCurrenciesBinding.inflate(layoutInflater)
+    }
+
+    private val viewModel by viewModels<CurrenciesViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        setupObserver()
+    }
+
+    private fun setupObserver() {
+        //viewModel.fetchCurrencies()
+
+        renderLoading()
+
+        viewModel.currencies.observe(this) { response ->
+            when (response) {
+                is Loading -> renderLoading()
+                is Content -> renderContent(data = response.content)
+                is Error -> renderError(throwable = response.throwable)
+            }
+        }
+    }
+
+    private fun renderLoading() {
+        binding.currenciesLoadingProgressbar.visibility = View.VISIBLE
+    }
+
+    private fun renderContent(data: CurrenciesVO) {
+        // render content
+        binding.currenciesLoadingProgressbar.visibility = View.GONE
+    }
+
+    private fun renderError(throwable: Throwable) {
+        // render error
+        binding.currenciesLoadingProgressbar.visibility = View.GONE
+    }
+}
