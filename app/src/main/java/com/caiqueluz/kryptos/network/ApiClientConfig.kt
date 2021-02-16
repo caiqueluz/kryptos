1399a7d56e5b6f6ad0d7fc4c3ff5f4891a5e8660
caiqueluz <eucaiqueluz@gmail.com>
2021-02-15 21:15:42 -0300

    add OkHttp client factory
    Signed-off-by: caiqueluz <eucaiqueluz@gmail.com>

--- app/src/main/java/com/caiqueluz/kryptos/network/ApiClientConfig.kt
+++ app/src/main/java/com/caiqueluz/kryptos/network/ApiClientConfig.kt
@@ -1,16 +1,19 @@
 package com.caiqueluz.kryptos.network
 
+import okhttp3.OkHttpClient
 import retrofit2.Converter
 import retrofit2.Retrofit
 
 private const val BASE_URL = "WIP"
 
 class ApiClientConfig(
-        private val converterFactory: Converter.Factory
+        private val converterFactory: Converter.Factory,
+        private val okHttpClient: OkHttpClient
 ) {
 
     fun create(): Retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(converterFactory)
+            .client(okHttpClient)
             .build()
 }
---
+++ app/src/main/java/com/caiqueluz/kryptos/network/OkHttpClientFactory.kt
@@ -0,0 +1,13 @@
+package com.caiqueluz.kryptos.network
+
+import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
+
+private const val READ_TIMEOUT = 30L
+
+class OkHttpClientFactory {
+
+    fun create(): OkHttpClient = OkHttpClient.Builder()
+            .readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)
+            .build()
+}
