795941cc112c528faf6f8c5c2283c8f681c9aaa7
caiqueluz <eucaiqueluz@gmail.com>
2021-02-21 15:27:35 -0300

    move NetworkResponseExtensions functions to NetworkResponseLiveData
    Signed-off-by: caiqueluz <eucaiqueluz@gmail.com>

--- app/src/main/java/com/caiqueluz/kryptos/network/NetworkResponseLiveData.kt
+++ app/src/main/java/com/caiqueluz/kryptos/network/NetworkResponseLiveData.kt
@@ -24,3 +24,22 @@ fun <RO, NRT> networkResponseLiveData(
 
     emit(networkResponse)
 }
+
+private fun <TYPE> Response<TYPE>.asNetworkResponse(): NetworkResponse<TYPE> =
+    when (this.isSuccessful) {
+        true -> buildNetworkResponse(content = this.body())
+        false -> buildErrorNetworkResponse()
+    }
+
+private fun <FROM, TO> NetworkResponse<FROM>.mapContent(
+    transformation: (FROM) -> TO
+): NetworkResponse<TO> = when (this) {
+    is NetworkResponse.Loading -> this
+    is NetworkResponse.Content -> NetworkResponse.Content(
+        content = transformation.invoke(this.content)
+    )
+    is NetworkResponse.Error -> {
+        error.cause?.printStackTrace()
+        NetworkResponse.Error(error = error)
+    }
+}
