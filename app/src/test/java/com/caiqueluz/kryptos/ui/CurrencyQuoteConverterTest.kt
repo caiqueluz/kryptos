1538a0492ad163285fbde3f951d9db3aa5c5b6d2
caiqueluz <eucaiqueluz@gmail.com>
2021-03-06 18:21:27 -0300

    add date tests on CurrencyQuoteConverter
    Signed-off-by: caiqueluz <eucaiqueluz@gmail.com>

--- app/src/test/java/com/caiqueluz/kryptos/ui/CurrencyQuoteConverterTest.kt
+++ app/src/test/java/com/caiqueluz/kryptos/ui/CurrencyQuoteConverterTest.kt
@@ -3,6 +3,8 @@ package com.caiqueluz.kryptos.ui
 import com.caiqueluz.kryptos.data.domain.CurrencyQuoteDTO
 import com.caiqueluz.kryptos.data.domain.CurrencyUsdPriceDTO
 import com.caiqueluz.kryptos.ui.converter.CurrencyQuoteConverter
+import com.caiqueluz.kryptos.ui.converter.DateConverter
+import com.caiqueluz.kryptos.ui.converter.DateFormatFactory
 import com.nhaarman.mockitokotlin2.eq
 import com.nhaarman.mockitokotlin2.spy
 import com.nhaarman.mockitokotlin2.verify
@@ -21,12 +23,16 @@ class CurrencyQuoteConverterTest {
     )
 
     private val converter = CurrencyQuoteConverter(
-        numberFormatter = spyNumberFormatter
+        numberFormatter = spyNumberFormatter,
+        dateConverter = DateConverter(
+            formatFactory = DateFormatFactory()
+        )
     )
 
     private val fakeCurrencyQuoteDTO = CurrencyQuoteDTO(
         priceInUsd = CurrencyUsdPriceDTO(
-            price = 1234.56
+            price = 1234.56,
+            lastUpdatedDate = "2021-03-06T20:29:02.000Z"
         )
     )
 
@@ -41,6 +47,14 @@ class CurrencyQuoteConverterTest {
     fun whenConvertQuoteIsCalled_verifyResponseHasCorrectFormattedPrice() {
         val expected = "U$1,234.56"
         val actual = converter.convertQuote(fakeCurrencyQuoteDTO).priceInUsd.price
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun whenConvertQuoteIsCalled_verifyResponseHasCorrectFormattedDate() {
+        val expected = "08:29, em 06 de Março"
+        val actual = converter.convertQuote(fakeCurrencyQuoteDTO).priceInUsd.lastUpdatedDate
 
         assertEquals(expected, actual)
     }
