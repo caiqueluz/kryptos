6d894bab5199ef7e07f65b57d095fd8b338bcec0
caiqueluz <eucaiqueluz@gmail.com>
2024-01-22 00:14:02 -0300

    test: add ServerResponseFakerExtensions

---
+++ app/src/test/java/com/caiqueluz/kryptos/ServerResponseFakerExtensions.kt
@@ -0,0 +1,86 @@
+package com.caiqueluz.kryptos
+
+fun ServerResponseFaker.fakeSuccessListingResponse(
+    delay: Long? = null
+) {
+    this
+        .on(path = "/v1/cryptocurrency/listings/latest?limit=50")
+        .answer(code = 200, body = listingSuccessResponseBody())
+        .apply {
+            delay?.let { delay(durationInSeconds = it) }
+        }
+        .register()
+
+    this
+        .on(path = "/v1/cryptocurrency/info?id=1%2C2%2C3")
+        .answer(code = 200, body = imagesSuccessResponseBody())
+        .register()
+}
+
+fun ServerResponseFaker.fakeErrorListingResponse() {
+    this
+        .on(path = "/v1/cryptocurrency/listings/latest?limit=50")
+        .answer(code = 404, body = null)
+        .register()
+}
+
+private fun listingSuccessResponseBody() =
+    """
+                {
+                    "data": [
+                        {
+                            "id": 1,
+                            "name": "crypto-1",
+                            "symbol": "FKC",
+                            "quote": {
+                                "USD": {
+                                    "price": 123,
+                                    "last_updated": "2020-01-01T00:30:00.000Z"
+                                }
+                            }
+                        },
+                        {
+                            "id": 2,
+                            "name": "crypto-2",
+                            "symbol": "FKC",
+                            "quote": {
+                                "USD": {
+                                    "price": 123,
+                                    "last_updated": "2020-01-01T00:30:00.000Z"
+                                }
+                            }
+                        },
+                        {
+                            "id": 3,
+                            "name": "crypto-3",
+                            "symbol": "FKC",
+                            "quote": {
+                                "USD": {
+                                    "price": 123,
+                                    "last_updated": "2020-01-01T00:30:00.000Z"
+                                }
+                            }
+                        }
+                    ]
+                }
+            """
+
+private fun imagesSuccessResponseBody() =
+    """
+            {
+                "data": {
+                    "1": {
+                        "id": 1,
+                        "logo": "http://localhost:8080/fake-image-url-1.png"
+                    },
+                    "2": {
+                        "id": 2,
+                        "logo": "http://localhost:8080/fake-image-url-2.png"
+                    },
+                    "3": {
+                        "id": 3,
+                        "logo": "http://localhost:8080/fake-image-url-3.png"
+                    }
+                }
+            }
+        """
