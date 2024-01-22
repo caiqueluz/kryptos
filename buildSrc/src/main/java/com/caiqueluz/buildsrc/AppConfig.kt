8725685c19d1b13688cacfb04dc065c1498b9cd0
caiqueluz <eucaiqueluz@gmail.com>
2024-01-22 20:13:20 -0300

    build: update compile sdk and target sdk to 34

--- buildSrc/src/main/java/com/caiqueluz/buildsrc/AppConfig.kt
+++ buildSrc/src/main/java/com/caiqueluz/buildsrc/AppConfig.kt
@@ -4,11 +4,11 @@ object AppConfig {
 
     const val APPLICATION_ID = "com.caiqueluz.kryptos"
 
-    const val COMPILE_SDK = 31
+    const val COMPILE_SDK = 34
     const val BUILD_TOOLS = "30.0.3"
 
     const val MIN_SDK = 21
-    const val TARGET_SDK = 31
+    const val TARGET_SDK = 34
     const val VERSION_CODE = 1
     const val VERSION_NAME = "1.0"
 
