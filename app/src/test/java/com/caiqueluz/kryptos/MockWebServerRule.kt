65e59a36a31ed3129ee64e4d2679d38c9ee45f1b
caiqueluz <eucaiqueluz@gmail.com>
2024-01-22 00:09:07 -0300

    test: add MockWebServerRule

---
+++ app/src/test/java/com/caiqueluz/kryptos/MockWebServerRule.kt
@@ -0,0 +1,27 @@
+package com.caiqueluz.kryptos
+
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+class MockWebServerRule(
+    private val faker: ServerResponseFaker
+) : TestWatcher() {
+
+    private lateinit var server: MockWebServer
+
+    override fun starting(description: Description) {
+        super.starting(description)
+
+        server = MockWebServer().apply {
+            dispatcher = faker
+            start(8080)
+        }
+    }
+
+    override fun finished(description: Description) {
+        super.finished(description)
+
+        server.shutdown()
+    }
+}
