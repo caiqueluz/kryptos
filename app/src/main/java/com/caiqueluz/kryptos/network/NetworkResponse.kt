6505b7379dc9a79aeecb98775b048820394a78b7
caiqueluz <eucaiqueluz@gmail.com>
2024-01-22 21:04:19 -0300

    refactor: update simple objects to data objects

--- app/src/main/java/com/caiqueluz/kryptos/network/NetworkResponse.kt
+++ app/src/main/java/com/caiqueluz/kryptos/network/NetworkResponse.kt
@@ -2,9 +2,9 @@ package com.caiqueluz.kryptos.network
 
 sealed class NetworkResponse<out TYPE> {
 
-    object Loading : NetworkResponse<Nothing>()
+    data object Loading : NetworkResponse<Nothing>()
 
     data class Content<TYPE>(val content: TYPE) : NetworkResponse<TYPE>()
 
-    object Error : NetworkResponse<Nothing>()
+    data object Error : NetworkResponse<Nothing>()
 }
--- app/src/main/java/com/caiqueluz/kryptos/ui/vo/HomeItemVO.kt
+++ app/src/main/java/com/caiqueluz/kryptos/ui/vo/HomeItemVO.kt
@@ -7,9 +7,9 @@ sealed class HomeItemVO(
     @StringRes val tabResId: Int
 ) {
 
-    object Currencies : HomeItemVO(tabResId = R.string.screen_currencies_title)
+    data object Currencies : HomeItemVO(tabResId = R.string.screen_currencies_title)
 
-    object News : HomeItemVO(tabResId = R.string.screen_news_title)
+    data object News : HomeItemVO(tabResId = R.string.screen_news_title)
 
-    object Settings : HomeItemVO(tabResId = R.string.screen_settings_title)
+    data object Settings : HomeItemVO(tabResId = R.string.screen_settings_title)
 }
